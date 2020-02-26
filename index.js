b074b9d72a9c78d7cb4b50b438d32c4bc5fa4700
Insaf <hamzin730insaf@gmail.com>
2020-02-25 20:34:01 -0500

    local settings

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const Telegraf = require('telegraf');
 const express = require('express');
 const serverless = require('serverless-http');
 
-const {textHandler} = require('./src/bot/text-handler');
-const {commandHandler} = require('./src/bot/command-handler');
-const {inlineHandler} = require('./src/bot/inline-handler');
+const { textHandler } = require('./src/bot/text-handler');
+const { commandHandler } = require('./src/bot/command-handler');
+const { inlineHandler } = require('./src/bot/inline-handler');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
@@ -19,19 +19,22 @@ commandHandler(bot);
 //Text messages handler
 textHandler(bot);
 
-//bot.launch();
 // bot.startWebhook(`/bot${process.env.BOT_TOKEN}`);
 
-const app = express();
+if (process.env.IS_LOCAL) {
+    bot.launch();
+    console.log('running locally..');
+} else {
+    const app = express();
 
-const secretPath = `/bot${process.env.BOT_TOKEN}`;
+    const secretPath = `/bot${process.env.BOT_TOKEN}`;
 
-app.use(bot.webhookCallback(secretPath));
-bot.telegram.setWebhook(`${process.env.HOOK_URL}${secretPath}`);
+    app.use(bot.webhookCallback(secretPath));
+    bot.telegram.setWebhook(`${process.env.HOOK_URL}${secretPath}`);
 
-app.get('/', (req, res)=>{
-    res.send('Bot is running...');
-});
+    app.get('/', (req, res) => {
+        res.send('Bot is running...');
+    });
 
-
-module.exports.start = serverless(app);
+    module.exports.start = serverless(app);
+}
